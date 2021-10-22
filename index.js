6713c2f1eacd33f47145373763d72af75f9b9bf9
rased100 <rased100@gmail.com>
2021-10-22 15:17:23 +0600

    64-3 Connect to cloud database and Send data to database

--- index.js
+++ index.js
@@ -12,20 +12,42 @@ const uri = "mongodb+srv://mydbuser1:RYoLm6Lv3TeX37cQ@cluster0.i4mso.mongodb.net
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
+// client.connect(err => {
+//     const collection = client.db("foodMaster").collection("users");
+//     // perform actions on the collection object
 
-    // console.log('Hitting the database')
-    // const user = { name: 'Mahiya Mahi', email: 'mahi@gmail.com', phone: '01999999999' }
-    // collection.insertOne()
-    //     .then(() => {
-    //         console.log('insert success')
-    //         // console.error(err)
-    //     })
+//     console.log('Hitting the database')
+//     const user = { name: 'Opu Biswash', email: 'opu@gmail.com', phone: '01999999999' }
+//     collection.insertOne(user)
+//         .then(() => {
+//             console.log('insert success')
+//             // console.error(err)
+//         })
 
-    client.close();
-});
+//     // client.close();
+// });
+
+// -------or-----------
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("foodMaster");
+        const usersCollection = database.collection("users");
+        // create a document to insert
+        const doc = {
+            name: "Spetial One",
+            email: "special@hotmail.com",
+        }
+        const result = await usersCollection.insertOne(doc);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+// -------or-----------
+
+
 
 app.get('/', (req, res) => {
     res.send('Running my CRUD Server')
